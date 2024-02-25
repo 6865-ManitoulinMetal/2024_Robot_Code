6310eeac0fb8177d2e6599033537912a58b81574
arachne-teach <93959864+arachne-teach@users.noreply.github.com>
2024-02-25 18:48:39 -0500

    added ids to constants.java

--- Robot2024/Robot2024/src/main/java/frc/robot/Constants.java
+++ Robot2024/Robot2024/src/main/java/frc/robot/Constants.java
@@ -18,29 +18,29 @@ package frc.robot;
 public class Constants {
     // Front Left Swerve Module Constants
     public static final int DRIVE_MOTOR_FRONT_LEFT_ID = 1;
-    public static final int ROTATION_MOTOR_FRONT_LEFT_ID = 5;
+    public static final int ROTATION_MOTOR_FRONT_LEFT_ID = 0;
     public static final int ENCODER_FRONT_LEFT_ID = 1;
     public static final double ENCODER_FRONT_LEFT_OFFSET = 0.0;
     public static final boolean ENCODER_FRONT_LEFT_INVERT = false;
 
     // Front Right Swerve Module Constants
     public static final int DRIVE_MOTOR_FRONT_RIGHT_ID = 2;
-    public static final int ROTATION_MOTOR_FRONT_RIGHT_ID = 6;
+    public static final int ROTATION_MOTOR_FRONT_RIGHT_ID = 3;
     public static final int ENCODER_FRONT_RIGHT_ID = 2;
     public static final double ENCODER_FRONT_RIGHT_OFFSET = 0.0;
     public static final boolean ENCODER_FRONT_RIGHT_INVERT = false;
 
     // Rear Left Swerve Module Constants
-    public static final int DRIVE_MOTOR_REAR_LEFT_ID = 3;
-    public static final int ROTATION_MOTOR_REAR_LEFT_ID = 7;
-    public static final int ENCODER_REAR_LEFT_ID = 3;
+    public static final int DRIVE_MOTOR_REAR_LEFT_ID = 4;
+    public static final int ROTATION_MOTOR_REAR_LEFT_ID = 5;
+    public static final int ENCODER_REAR_LEFT_ID = 4;
     public static final double ENCODER_REAR_LEFT_OFFSET = 0.0;
     public static final boolean ENCODER_REAR_LEFT_INVERT = false;
 
     // Rear Right Swerve Module Constants
-    public static final int DRIVE_MOTOR_REAR_RIGHT_ID = 4;
-    public static final int ROTATION_MOTOR_REAR_RIGHT_ID = 8;
-    public static final int ENCODER_REAR_RIGHT_ID = 4;
+    public static final int DRIVE_MOTOR_REAR_RIGHT_ID = 6;
+    public static final int ROTATION_MOTOR_REAR_RIGHT_ID = 7;
+    public static final int ENCODER_REAR_RIGHT_ID = 3;
     public static final double ENCODER_REAR_RIGHT_OFFSET = 0.0;
     public static final boolean ENCODER_REAR_RIGHT_INVERT = false;
 
@@ -49,7 +49,7 @@ public class Constants {
 
     // Gear ratios for swerve modules
     // Sample gear ratio for MKvi L3 swerve module
-    public static final double GEAR_RATIO_MKVI_L3 = 12.75; // Example gear ratio for MKvi L3 swerve module
+    public static final double GEAR_RATIO_MKVI_L3 = 6.12; // overall gear ratio for MKvi L3 swerve module
 
     public static final double DEFAULT_DRIVE_SPEED = 0.2; // Default drive speed 0.2 unless overwritten in commands
     public static final int TRACK_WIDTH = 0;
