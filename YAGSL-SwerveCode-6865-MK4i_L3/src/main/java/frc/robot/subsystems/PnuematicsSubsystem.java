9bbde980c30afb5aa08f065dfc16b94149161b9c
leadProgrammerRyan6865 <122568567+leadProgrammerRyan6865@users.noreply.github.com>
2024-02-28 14:48:15 -0500

    Add pnematics system

---
+++ YAGSL-SwerveCode-6865-MK4i_L3/src/main/java/frc/robot/subsystems/PnuematicsSubsystem.java
@@ -0,0 +1,72 @@
+package frc.robot.subsystems;
+
+
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
+
+public class PnuematicsSubsystem extends SubsystemBase 
+{
+    public DoubleSolenoid doubleSolenoid1;
+
+    
+
+    public PnuematicsSubsystem(int pcmId, int solenoidChannel0, int solenoidChannel1, int solenoidChannel2, int solenoidChannel3) 
+    {
+        this.doubleSolenoid1 = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1);
+    }
+   
+   
+    // Method to raise both pneumatic systems
+    public void raise() 
+    {
+        doubleSolenoid1.set(DoubleSolenoid.Value.kForward);
+    }
+   
+   
+   
+   
+    // Method to lower pneumatics and 
+    public void lower()
+    {
+        doubleSolenoid1.set(DoubleSolenoid.Value.kReverse);
+    }
+   
+   
+    // Method to check if both pneumatic systems are raised
+    public boolean areRaised() 
+    {
+        return doubleSolenoid1.get() == DoubleSolenoid.Value.kForward;
+    }
+   
+   
+    // Method to check if both pneumatic systems are lowered
+    public boolean areLowered() 
+    {
+        return doubleSolenoid1.get() == DoubleSolenoid.Value.kReverse;
+    }
+   
+   
+    // Example of how to toggle the state
+    public void toggle() 
+    {
+        if (areRaised()) 
+        {
+            lower();
+        } 
+        else 
+        {
+            raise();
+        }
+    }
+   
+
+    @Override
+    public void periodic()
+    { 
+        
+    }    
+}
