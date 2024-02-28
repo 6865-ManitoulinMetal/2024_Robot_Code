1087d79e1ed6a058607063dbc6724bfb8d9eb413
leadProgrammerRyan6865 <122568567+leadProgrammerRyan6865@users.noreply.github.com>
2024-02-28 13:00:37 -0500

    New YAGSL swerve code

---
+++ YAGSL-SwerveCode-6865-MK4i_L3/src/main/java/frc/robot/commands/swervedrive/drivebase/Drive.java
@@ -0,0 +1,92 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.swervedrive.drivebase;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+import java.util.List;
+import java.util.function.DoubleSupplier;
+import swervelib.SwerveController;
+import swervelib.math.SwerveMath;
+
+/**
+ * An example command that uses an example subsystem.
+ */
+public class Drive extends Command
+{
+
+  private final SwerveSubsystem swerve;
+  private final DoubleSupplier  vX, vY;
+  private final DoubleSupplier rX;
+  private boolean initRotation = false;
+
+  /**
+   * Used to drive a swerve robot in full field-centric mode.  vX and vY supply translation inputs, where x is
+   * torwards/away from alliance wall and y is left/right. headingHorzontal and headingVertical are the Cartesian
+   * coordinates from which the robot's angle will be derived— they will be converted to a polar angle, which the robot
+   * will rotate to.
+   *
+   * @param swerve            The swerve drivebase subsystem.
+   * @param vX                DoubleSupplier that supplies the x-translation joystick input.  Should be in the range -1
+   *                          to 1 with deadband already accounted for.  Positive X is away from the alliance wall.
+   * @param vY                DoubleSupplier that supplies the y-translation joystick input.  Should be in the range -1
+   * @param rX                       to 1 with deadband already accounted for.  Positive Y is towards the left wall when
+   *                          looking through the driver station glass.
+   *   DoubleSupplier that supplies the vertical component of the robot's heading angle.  In the
+   *                          robot coordinate system, this is along the same axis as vX.  Should range from -1 to 1
+   *                          with no deadband. Positive is away from the alliance wall.
+   */
+  public Drive(SwerveSubsystem swerve, DoubleSupplier vX, DoubleSupplier vY, DoubleSupplier rX)
+  {
+    this.swerve = swerve;
+    this.vX = vX;
+    this.vY = vY;
+   this.rX = rX;
+
+    addRequirements(swerve);
+  }
+
+  @Override
+  public void initialize()
+  {
+    initRotation = true;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute()
+  {
+
+
+
+  
+
+    // Limit velocity to prevent tippy
+    
+    // Make the robot move
+    swerve.driveCommand(vX, vY, rX);
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted)
+  {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished()
+  {
+    return false;
+  }
+
+
+}
