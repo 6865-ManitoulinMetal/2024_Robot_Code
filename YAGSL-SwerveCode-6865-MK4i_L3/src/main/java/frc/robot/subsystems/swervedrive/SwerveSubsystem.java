9b8eaf2fefe48442530a4e0f4b6dfa05e6c3c5a1
leadProgrammerRyan6865 <122568567+leadProgrammerRyan6865@users.noreply.github.com>
2024-02-29 21:19:30 -0500

    New configs

--- YAGSL-SwerveCode-6865-MK4i_L3/src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ YAGSL-SwerveCode-6865-MK4i_L3/src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -19,6 +19,7 @@ import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Config;
@@ -309,6 +310,7 @@ public class SwerveSubsystem extends SubsystemBase
   @Override
   public void periodic()
   {
+   // System.out.println(swerveDrive.getModules()[0].getAngleMotor());
   }
 
   @Override
