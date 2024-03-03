65aef9ac35fa04680fe03e2e9c9f77c3d0c3bd22
leadProgrammerRyan6865 <122568567+leadProgrammerRyan6865@users.noreply.github.com>
2024-03-02 23:05:20 -0500

    New swere test code

---
+++ BaseTalonFXSwerve-main/src/main/java/frc/robot/subsystems/PnuematicsSubsystem.java
@@ -0,0 +1,78 @@
+package frc.robot.subsystems;
+
+
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
+
+public class PnuematicsSubsystem extends SubsystemBase 
+{
+    public DoubleSolenoid doubleSolenoid1;
+    public DoubleSolenoid doubleSolenoid2;
+
+    
+
+    public PnuematicsSubsystem(int pcmId, int solenoidChannel0, int solenoidChannel1, int solenoidChannel2, int solenoidChannel3) 
+    {
+        this.doubleSolenoid1 = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1);
+        this.doubleSolenoid2 = new DoubleSolenoid(PneumaticsModuleType.REVPH, 2, 3);
+    }
+   
+   
+    // Method to raise both pneumatic systems
+    public void raise() 
+    {
+        doubleSolenoid1.set(DoubleSolenoid.Value.kForward);
+        doubleSolenoid2.set(DoubleSolenoid.Value.kForward);
+    }
+   
+   
+   
+   
+    // Method to lower both pneumatic systems
+    public void lower()
+    {
+        doubleSolenoid1.set(DoubleSolenoid.Value.kReverse);
+        doubleSolenoid2.set(DoubleSolenoid.Value.kReverse);
+    }
+   
+   
+    // Method to check if both pneumatic systems are raised
+    public boolean areRaised() 
+    {
+        return doubleSolenoid1.get() == DoubleSolenoid.Value.kForward &&
+               doubleSolenoid2.get() == DoubleSolenoid.Value.kForward;
+    }
+   
+   
+    // Method to check if both pneumatic systems are lowered
+    public boolean areLowered() 
+    {
+        return doubleSolenoid1.get() == DoubleSolenoid.Value.kReverse &&
+               doubleSolenoid2.get() == DoubleSolenoid.Value.kReverse;
+    }
+   
+   
+    // Example of how to toggle the state
+    public void toggle() 
+    {
+        if (areRaised()) 
+        {
+            lower();
+        } 
+        else 
+        {
+            raise();
+        }
+    }
+   
+
+    @Override
+    public void periodic()
+    { 
+        
+    }    
+}
