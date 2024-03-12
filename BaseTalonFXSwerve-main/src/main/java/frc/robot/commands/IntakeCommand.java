f0acb04104000768387af95c7e9ced56b7b9ad58
leadProgrammerRyan6865 <122568567+leadProgrammerRyan6865@users.noreply.github.com>
2024-03-12 19:10:36 -0400

    Working code

--- BaseTalonFXSwerve-main/src/main/java/frc/robot/commands/IntakeCommand.java
+++ BaseTalonFXSwerve-main/src/main/java/frc/robot/commands/IntakeCommand.java
@@ -5,7 +5,6 @@ import frc.robot.subsystems.HolsterSubsystem;
 
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 
 public class IntakeCommand extends Command {    
