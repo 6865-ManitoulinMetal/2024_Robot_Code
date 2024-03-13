edc90161daae8c18d52b99c978b4e4136d48a3ca
dylan8080~ <kunt8405@rscloud.ca>
2024-03-12 22:37:11 -0400

    added Tanner's Light Beam code

--- BaseTalonFXSwerve-main/src/main/java/frc/robot/Robot.java
+++ BaseTalonFXSwerve-main/src/main/java/frc/robot/Robot.java
@@ -7,6 +7,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -30,7 +32,10 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-  }
+
+    UsbCamera Camera1 = CameraServer.startAutomaticCapture();
+    UsbCamera Camera2 = CameraServer.startAutomaticCapture();
+    }
 
   /**
    * This function is called every robot packet, no matter the mode. Use this for items like
